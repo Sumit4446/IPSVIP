6921f27ecd47bd0c42a14e21a27757018207dccd
Sumit V Sakure <95362900+Sumit4446@users.noreply.github.com>
2022-01-08 19:37:28 +0530

    initial commit

---
+++ Task2/shoppingcart/src/shoppingcart/Cart.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shoppingcart;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author Sumit
+ */
+public class Cart {
+     List<Product> cartItems = new ArrayList<Product>();
+    
+    public void addProductToCartByPID(int pid) {
+        Product product = getProductByProductID(pid);
+        addToCart(product);
+    }
+
+    private Product getProductByProductID(int pid) {
+        Product product = null;
+        List<Product> products = new Products().getProducts();
+        for (Product prod: products) {
+            if (prod.getPid() == pid) {
+                product = prod;
+                break;
+            }
+        }
+        return product;
+    }
+
+    private void addToCart(Product product) {
+        cartItems.add(product);
+    }
+
+    public void removeProductByPID(int pid) {
+        Product prod = getProductByProductID(pid);
+        cartItems.remove(prod);
+    }
+
+    void printCartItems() {
+        for (Product prod: cartItems) {
+            System.out.println(prod.getName());
+        }
+    } 
+}
